fbdf520d4f17d4d9c927764c676d54fbf66ff269
Denis Soares <denissoarespereira02@gmail.com>
2023-11-02 23:07:08 -0300

    feat: creating User Manager

--- src/layout/SideBar/components/MenuList/index.jsx
+++ src/layout/SideBar/components/MenuList/index.jsx
@@ -1,34 +1,44 @@
-import { Menu } from 'antd'
-import {HouseSimple, ChartBarHorizontal, ChartLineUp, Money, Archive } from '@phosphor-icons/react' 
-import React from 'react'
-import * as S from './styles'
+import { Menu } from "antd";
+import {
+  HouseSimple,
+  ChartBarHorizontal,
+  ChartLineUp,
+  Money,
+  Archive,
+} from "@phosphor-icons/react";
+import React from "react";
+import * as S from "./styles";
 
 const MenuList = () => {
-  const {Item,Divider} = Menu
+  const { Item, Divider } = Menu;
   return (
-    <S.StyledMenu theme='light' mode='inline'>
-      <Item  key='Home' icon={<HouseSimple size={20} />}>
-        <a href='/'>Dashboard</a>
+    <S.StyledMenu theme="light" mode="inline">
+      <Item key="Home" icon={<HouseSimple size={20} />}>
+        <a href="/">Dashboard</a>
       </Item>
       <Divider />
-      <Item key='Vendas' icon={<ChartBarHorizontal size={20} />}>
-        <a href='/vendas'>Vendas</a>
+      <Item key="Vendas" icon={<ChartBarHorizontal size={20} />}>
+        <a href="/vendas">Vendas</a>
       </Item>
       <Divider />
-      <Item key='DesempenhoPessoal' icon={<ChartLineUp size={20} />}>
-        <a href='/desempenhoPessoal'>Desempenho Pessoal </a>
+      <Item key="DesempenhoPessoal" icon={<ChartLineUp size={20} />}>
+        <a href="/desempenhoPessoal">Desempenho Pessoal </a>
       </Item>
       <Divider />
-      <Item key='Financeiro' icon={<Money size={20} />}>
-        <a href='/financeiro'>Financeiro</a>
+      <Item key="Financeiro" icon={<Money size={20} />}>
+        <a href="/financeiro">Financeiro</a>
       </Item>
       <Divider />
-      <Item key='Inventario&Estoque' icon={<Archive size={20} />}>
-        <a href='/inventarioeestoque'>Inventario e Estoque</a>
+      <Item key="Inventario&Estoque" icon={<Archive size={20} />}>
+        <a href="/inventarioeestoque">Inventario e Estoque</a>
+      </Item>
+      <Divider />
+      <Item key="GerneciadorDeUsuario" icon={<Archive size={20} />}>
+        <a href="/gerenciadordeusuario">Gerenciador de Usuário</a>
       </Item>
       <Divider />
     </S.StyledMenu>
-  )
-}
+  );
+};
 
-export default MenuList
+export default MenuList;
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import "../index.css";
 import Dashboard from "../modules/home";
+import UserManager from "../modules/userManager";
 
 const AppRoutes = () => {
   return (
@@ -10,6 +11,7 @@ const AppRoutes = () => {
       <Route path="/desempenhopessoal" element={<> Desempenhopessoal </>} />
       <Route path="/financeiro" element={<> financeiro </>} />
       <Route path="/inventarioeestoque" element={<> inventarioeestoque </>} />
+      <Route path="/gerenciadordeusuario" element={<UserManager />} />
     </Routes>
   );
 };
