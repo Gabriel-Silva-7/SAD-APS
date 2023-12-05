e67c48cae288d12c5f22e07e7e1046eaa19a5ec5
Gabriel Silva <66033771+Gabriel-Silva-7@users.noreply.github.com>
2023-12-04 22:04:06 -0300

    hotfix

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -24,6 +24,7 @@ export const AuthProvider = ({ children }) => {
   const logout = () => {
     localStorage.removeItem("token");
     setAuthenticated(false);
+    window.location.reload();
   };
 
   return (
