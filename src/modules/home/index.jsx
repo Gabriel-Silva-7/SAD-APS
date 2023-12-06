43faeac17574a96ab4b9424923f5ceb7481c9d36
Gabriel Silva <66033771+Gabriel-Silva-7@users.noreply.github.com>
2023-12-05 23:48:18 -0300

    feat: Cards

--- src/modules/home/index.jsx
+++ src/modules/home/index.jsx
@@ -21,7 +21,6 @@ const Dashboard = () => {
   useEffect(() => {
     getNewClients();
   }, []);
-  console.log(newClients[0].NovosClientes);
 
   return (
     <div style={{ padding: "20px", width: "98%" }}>
@@ -34,7 +33,7 @@ const Dashboard = () => {
       </div>
       <S.GridContainer>
         <S.CardGrid>
-          {/* <Card
+          <Card
             title="Novos Clientes"
             date="Semanal"
             total={newClients && newClients[0]?.NovosClientes}
@@ -42,7 +41,7 @@ const Dashboard = () => {
           />
           <Card title="Activity" date="yearly" />
           <Card title="Real-Time" />
-          <Card title="Bounce" date="annual" /> */}
+          <Card title="Bounce" date="annual" />
         </S.CardGrid>
         <S.ChartContainer>
           <BarCharts selectedOption={selectedOption} />
